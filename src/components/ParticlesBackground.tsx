54f90a1a745d0ed73e06c5370260d2ae880e9bc8
Hans <hans.villarroel@usach.cl>
2022-12-29 04:08:50 -0300

    [Feat]: Improved responsive image and layout

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -20,82 +20,84 @@ export default function ParticlesBackground() {
     []
   );
   return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        background: {
-          color: {
-            value: "bg-zinc-900",
-          },
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: false,
-              mode: "push",
+    <div className="fixed">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "bg-zinc-900",
             },
-            onHover: {
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
               enable: true,
-              mode: "repulse",
+              opacity: 0.5,
+              width: 1,
             },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 1.2,
-            straight: false,
-          },
-          number: {
-            density: {
+            collisions: {
               enable: true,
-              area: 800,
             },
-            value: 80,
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 1.2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
           },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 5 },
-          },
-        },
-        detectRetina: true,
-      }}
-    />
+          detectRetina: true,
+        }}
+      />
+    </div>
   );
 }
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -2,6 +2,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { useEffect, useState } from "react";
 
+const privateInfo = {
+  name: "Hans Villarroel",
+  description:
+    "Hola, soy Hans, un estudiante actualmente cursando Ingenieria " +
+    "ejecucion en la Universidad de Santiago de Chile",
+};
+
 function isMobile(): boolean {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
@@ -37,40 +44,52 @@ function isMobile(): boolean {
   return true;
 }
 
+const nameDiv = () => {
+  return (
+    <h1 className="text-2xl md:text-5xl font-mono font-medium underline decoration-1 underline-offset-8 text-center py-4">
+      {privateInfo.name}
+    </h1>
+  );
+};
+
+const descriptionDiv = () => {
+  return (
+    <div>
+      <p className="text-justify px-4 ">{privateInfo.description} </p>
+    </div>
+  );
+};
+
+// Icons code
+// <div className="flex justify-center gap-10 ">
+//   <FontAwesomeIcon className=" h-6 md:h-16" icon={faGithub} />
+//   <FontAwesomeIcon className=" h-6 md: md:h-16" icon={faLinkedin} />
+// </div>
+
 export default function ProfileCard(): JSX.Element {
   const mobile = isMobile();
   return (
-    <div className="bg-zinc-800 h-5/6 w-5/6 -my-32 md:h-4/6 md:w-4/6 rounded-xl grid grid-cols-1 md:grid-cols-2 text-white backdrop-blur-lg bg-opacity-50 ">
+    <div className="bg-zinc-800 h-5/6 w-5/6 md:w-4/6 rounded-xl grid grid-cols-1 md:grid-cols-2 text-white backdrop-blur-lg bg-opacity-50 overflow-auto">
       <div className="grid grid-rows-3 place-items-center">
         {!mobile ? (
-          <h1 className="md:text-5xl font-mono font-medium underline decoration-1 underline-offset-8 ">
-            Hans Villarroel
-          </h1>
+          <div>
+            {" "}
+            <div>{nameDiv()}</div> <div> {descriptionDiv()} </div>{" "}
+          </div>
         ) : null}
       </div>
-      <div className="grid grid-rows-6 place-items-center overflow-hidden mx-auto py-16 -mb-24 md:py-0 md:-mb-0">
+      <div className="grid grid-rows-6 place-items-center py-8">
         <img
-          className="h-auto max-h-36 md:max-h-48 rounded-full mx-auto md:row-span-4 "
+          className="h-auto max-h-36 md:max-h-48 lg:max-h-64 rounded-full md:row-span-3 "
           src="https://avatars.githubusercontent.com/u/59944004?v=4"
         />
-
         {mobile ? (
-          <h1 className="text-2xl font-mono font-medium underline decoration-1 underline-offset-8">
-            Hans Villarroel
-          </h1>
-        ) : null}
-        <div>
-          <p className="text-justify px-4">
+          <div>
             {" "}
-            Hola, soy Hans, un estudiante actualmente cursando Ingenieria en
-            ejecucion en la Universidad de Santiago de Chile.
-          </p>
-        </div>
-
-        <div className="flex justify-center gap-10 ">
-          <FontAwesomeIcon className=" h-6 md:h-16" icon={faGithub} />
-          <FontAwesomeIcon className=" h-6 md: md:h-16" icon={faLinkedin} />
-        </div>
+            <div>{nameDiv()}</div>
+            <div> {descriptionDiv()} </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
