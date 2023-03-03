83906d9fb91df7991a9ae41f1339fb70c2116a59
Hans <hans.villarroel@usach.cl>
2023-03-03 00:40:24 -0300

    Different pictures for different devices

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,19 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { CSSProperties, useEffect, useState } from "react";
+import { faClipboard } from "@fortawesome/free-regular-svg-icons";
 
 const privateInfo = {
   name: "Hans Villarroel",
-  description: `Hola, soy Hans!     
+  description: `Hola, soy Hans y soy un desarrollador web fullstack.     
   Actualmente soy un estudiante cursando Ingenieria de ejecucion en computacion e informatica en la Universidad de Santiago de Chile.
   Me enfoco principalemente en desarrollo web usando diferentes tecnologias como React, Next, NodeJS y otros.`,
   github: "github.com/Henny35P",
   linkedin: "linkedin.com/in/hansvillarroel",
+  curriculum: "",
 };
 
 const nameDiv = () => {
   return (
-    <h1 className="text-2xl md:text-5xl font-mono font-medium underline decoration-1 underline-offset-8 text-center py-2 md:py-8 lg:py-12">
+    <h1 className="text-2xl md:text-5xl font-mono font-medium underline decoration-3 underline-offset-8 text-center py-2 md:py-8 lg:py-12">
       {privateInfo.name}
     </h1>
   );
@@ -22,7 +24,7 @@ const nameDiv = () => {
 const descriptionDiv = () => {
   return (
     <div>
-      <p className="px-8 whitespace-pre-line  text-xl md:text-2xl">
+      <p className="px-8 whitespace-pre-line text-xl md:text-2xl">
         {privateInfo.description}{" "}
       </p>
     </div>
@@ -32,6 +34,11 @@ const descriptionDiv = () => {
 const infoDiv = () => {
   return (
     <div className="flex justify-center gap-10 py-6 ">
+      <FontAwesomeIcon
+        className=" h-6 md:h-16 cursor-pointer"
+        icon={faClipboard}
+        onClick={() => handleClick(`https://${privateInfo.curriculum}`)}
+      />
       <FontAwesomeIcon
         className=" h-6 md:h-16 cursor-pointer"
         icon={faGithub}
@@ -94,7 +101,8 @@ export default function ProfileCard(): JSX.Element {
   const mobile = isMobile();
   return (
     <div className="grid place-items-center h-screen">
-      <div className="bg-zinc-800 h-5/6 w-5/6 md:w-4/6 rounded-xl grid grid-cols-1 md:grid-cols-3 text-white backdrop-blur-md bg-opacity-30 overflow-hidden shadow-black shadow-2xl">
+      <div className="bg-zinc-800 md:h-5/6 w-5/6 md:w-4/6 rounded-xl grid grid-cols-1 md:grid-cols-3 text-white backdrop-blur-md bg-opacity-30 shadow-black shadow-2xl ">
+        {/* render para desktops */}
         <div className="grid grid-rows-3 place-items-center col-span-2">
           {!mobile ? (
             <div className="md:row-span-2 md:px-0 lg:px-12">
@@ -103,20 +111,24 @@ export default function ProfileCard(): JSX.Element {
             </div>
           ) : null}
         </div>
-        <div className="grid grid-rows-6 place-items-center ">
-          <img
-            className="h-auto max-h-36 md:max-h-48 row-span-1 rounded-full md:row-span-4 "
-            src="https://avatars.githubusercontent.com/u/59944004?v=4"
-          />
-          {mobile ? (
-            <div className="row-span-2">
-              {" "}
-              <div>{nameDiv()}</div>
-              <div> {descriptionDiv()} </div>
-              {infoDiv()}
+        <img
+          className="object-scale-down my-16 -mx-8 rounded-md shadow-black shadow-2xl"
+          src="/lonmgtest.jpg"
+        />{" "}
+        {/* render para mobile */}
+        {mobile ? (
+          <div>
+            <div className="flex place-content-center">
+              <img
+                className="align-top max-h-44 rounded-full mt-4 "
+                src="https://avatars.githubusercontent.com/u/59944004?v=4"
+              />{" "}
             </div>
-          ) : null}
-        </div>
+            <div>{nameDiv()}</div>
+            <div> {descriptionDiv()} </div>
+            <div>{infoDiv()}</div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
